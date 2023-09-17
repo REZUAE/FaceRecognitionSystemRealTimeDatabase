4ce6f9aeeb62f65be3a2bac723ec9fdeb4b0b960
REZUAE <Jainvokher@gmail.com>
2023-09-17 20:09:30 +0500

    encodings update
    Current face doesnt recognize well

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ print("Loading encoding.... Sucesss")
 
 while True:
     success, img = cap.read()
-    imgS = cv2.resize(img,(0,0),None, 0.25,0.25)
+    imgS = cv2.resize(img,(0,0),None, 0.75,0.75)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
     faceCurFrame = face_recognition.face_locations(imgS)
